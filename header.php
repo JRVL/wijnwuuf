956804fdcd1a7d4fc0382b1e1e4ba87cb16337c0
Jerry van Luijk <JvanLuijk@mbp-van-jerry-2.home>
2019-04-09 08:01:25 +0200

    header

--- header.php
+++ header.php
@@ -10,7 +10,44 @@
     
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
 
+var j$ = jQuery,
+    $nav = j$(".main-navigation"),
+    $slideLine = j$("#slide-line"),
+    $currentItem = j$(".current-menu-item");
 
+j$(function(){  
+  // Menu has active item
+  if ($currentItem[0]) {
+    $slideLine.css({
+      "width": $currentItem.width(),
+      "left": $currentItem.position().left
+    });
+  }
+  
+  // Underline transition
+  j$($nav).find(".current-menu-item a").hover(
+    // Hover on
+    function(){
+      $slideLine.css({
+        "width": j$(this).width(),
+        "left": j$(this).position().left
+      });
+    },
+    // Hover out
+    function(){
+      if ($currentItem[0]) {
+        // Go back to current
+        $slideLine.css({
+          "width": $currentItem.width(),
+          "left": $currentItem.position().left
+        });
+      } else {
+        // Disapear
+        $slideLine.width(0);
+      }
+    }
+   );
+});
 
 
         
@@ -30,13 +67,16 @@
     
     <header class="header">
         <a href="index.php"><img src="<?php bloginfo ('template_url') ?>/images/logo.svg" alt="" class="logo"></a>
-        <div id="main-navigation">
+        <div class="main-navigation">
             <?php 
                     wp_nav_menu( $arg = array (
                         'menu_class' => 'main-navigation',
-                        'theme_location' => 'primary'
+                        'theme_location' => 'primary',
+                        'link_before' => '<span>',
+                        'link_after' => '</span>'
+                        
                     ))
             ?> 
-   
+             <span id="slide-line"></span>     
         </div>
     </header>
