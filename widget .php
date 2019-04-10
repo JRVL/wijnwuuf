f65ad2b6abdebc1710f3bc204017f1fb0274eb03
Jerry van Luijk <JvanLuijk@mbp-van-jerry-2.home>
2019-04-11 00:02:15 +0200

    widget

---
+++ widget .php
@@ -0,0 +1,9 @@
+<body>
+<div class="widgetfront1"><a href="http://localhost:8080/wordpress/?p=19">
+	<img src="http://localhost:8080/wordpress/wp-content/uploads/2019/04/68db818737075053481f581c4454a603dac98604_sierra.jpeg" alt="Avatar">
+	<h6 class="widget-title">
+		Shop</h6>
+<p class="widget-text">Hi! ik ben Amber, het wijnwuuf. Gek op alles wat met wijn heeft te maken. Meer over mij weten? 
+</p></a></div>
+    
+</body>
